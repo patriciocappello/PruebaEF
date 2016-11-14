33af9277c68974c69a9cb383cc7a1b626161855c
patriciocappello <pato91_boca@hotmail.com>
2016-11-14 20:37:23 -0300

    primer commit

---
+++ Ejemplo1/Entidades/Perfil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Perfil
+    {
+        public int IdPerfil { get; set; }
+        public string Nombre { get; set; }
+        public string Administrador { get; set; }
+    }
+}
---
+++ Ejemplo1/Entidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class Usuario
+    {
+        public string Login { get; set; }
+        public DateTime FechaUltimoLogin { get; set; }
+        public int IntentosInvalidos { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+}
